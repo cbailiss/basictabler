f9f49196fad19d1cd1886ffdc56d93b5b41ba64d
cbailiss <cbailiss@gmail.com>
2017-09-28 23:00:01 +0100

    Travis CI debugging.

--- R/quickTable.R
+++ R/quickTable.R
@@ -95,7 +95,7 @@ qhtbl <- function(dataFrameOrMatrix, columnNamesAsColumnHeaders=TRUE, explicitCo
   checkArgument(3, TRUE, "", "qhtbl", firstColumnAsRowHeaders, missing(firstColumnAsRowHeaders), allowMissing=TRUE, allowNull=FALSE, allowedClasses="logical")
   checkArgument(3, TRUE, "", "qhtbl", explicitRowHeaders, missing(explicitRowHeaders), allowMissing=TRUE, allowNull=TRUE, allowedClasses="character")
   checkArgument(3, TRUE, "", "qhtbl", columnFormats, missing(columnFormats), allowMissing=TRUE, allowNull=TRUE, allowedClasses=c("character", "list", "function"))
-  stop("cmbdebug02")
+  stop(paste0("cmbdebug02: ", class(arguments))
   argumentCheckMode <- arguments$argumentCheckMode
   warning("cmbdebug03")
   if(is.null(argumentCheckMode)) argumentCheckMode <- "auto"
